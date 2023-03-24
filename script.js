26970386ce0a1798c9fdd3de397ac286c657f225
Vahan Nadjarian <vahan@nadjarian.com>
2023-03-24 13:31:21 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content was added via Javascipt"
+document.querySelector("body").appendChild(h2);
+
+const num = document.createElement("p");
+num.textContent = "1234567890";
+document.querySelector("body").appendChild(num);
